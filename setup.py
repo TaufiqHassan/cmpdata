40f9b0871df88b8db9069cac4d51527d8e44c184
Taufiq <taufiq@vortex1.ucr.edu>
2020-09-11 14:27:34 -0700

    env added

--- setup.py
+++ setup.py
@@ -11,11 +11,6 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as readme_file:
     readme = readme_file.read()
 
 requirements = [
-    'numpy',
-    'pandas',
-    'xarray',
-    'dask',
-    'scipy',
     'pytest',
     'setuptools',
 ]
