8079c32e9c58af11547d3dd0313c9c782b05353e
TaufiqHassan <taufiq.hassan@pnnl.gov>
2022-08-17 14:41:31 -0700

    bump doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ author = 'Taufiq Hassan'
 master_doc = 'index'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.0.1'
 
 
 # -- General configuration ---------------------------------------------------
